0bf79efc4af1315c9008533cf73ba83a49d36465
sheeny84 <roisin.soulsby@gmail.com>
2025-09-05 12:33:03 +1000

    Swap file contents

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for i in range(0, 99):
-    print(f"{i} = 0x{i:x}")
+for i in 'abcdefghijklmnopqrstuvwxyz':
+    if i != 'e' and i != 'q':
+        print("{}".format(i), end="")
--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for i in 'abcdefghijklmnopqrstuvwxyz':
-    if i != 'e' and i != 'q':
-        print("{}".format(i), end="")
+for i in range(0, 99):
+    print("{} = 0x{:x}".format(i, i))
