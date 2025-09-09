3786dc3281e82f6c1ec8964fc998331a8f54ff9b
sheeny84 <roisin.soulsby@gmail.com>
2025-09-09 13:55:32 +1000

    Add handling for empty string to task 8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    first_char = sentence[0]
+    if length == 0:
+        first_char = None
+    else:
+        first_char = sentence[0]
     return length, first_char
