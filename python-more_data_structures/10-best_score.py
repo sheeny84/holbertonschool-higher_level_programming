c30ad4492f4b258d6eda5f05756f5fcb0d299bb1
sheeny84 <roisin.soulsby@gmail.com>
2025-09-10 15:55:15 +1000

    Add files for tasks 8-10

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        max_key = None
+    else:
+        max_val = a_dictionary[next(iter(a_dictionary))]
+        for k, v in a_dictionary.items():
+            if v > max_val:
+                max_val = v
+                max_key = k
+    return max_key
