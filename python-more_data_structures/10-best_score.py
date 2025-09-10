29613d1d08a1dda8a79960cfe0230de45b8bee7b
sheeny84 <roisin.soulsby@gmail.com>
2025-09-10 15:59:40 +1000

    Update task 10 to initialise max_key

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,7 +3,8 @@ def best_score(a_dictionary):
     if not a_dictionary:
         max_key = None
     else:
-        max_val = a_dictionary[next(iter(a_dictionary))]
+        max_key = next(iter(a_dictionary))
+        max_val = a_dictionary[max_key]
         for k, v in a_dictionary.items():
             if v > max_val:
                 max_val = v
