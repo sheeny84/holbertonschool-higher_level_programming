d16c9c30c80d1dfd9ec243a1368e6fb4d5012cae
sheeny84 <roisin.soulsby@gmail.com>
2025-09-10 18:17:50 +1000

    Add handling for x=0 to task 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    for i in range(0, x):
-        try:
-            print("{}".format(my_list[i]), end="")
-        except IndexError:
-            print("")
-            return i
-    print("")
-    return i + 1
+    if x == 0:
+        return 0
+    else:
+        for i in range(0, x):
+            try:
+                print("{}".format(my_list[i]), end="")
+            except IndexError:
+                print("")
+                return i
+        print("")
+        return i + 1
