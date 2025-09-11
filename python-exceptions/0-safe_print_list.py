a822703a698b61432f87079806e2e1cfaa4a88a2
sheeny84 <roisin.soulsby@gmail.com>
2025-09-11 13:27:05 +1000

    Update try and catch for task 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,11 +3,11 @@ def safe_print_list(my_list=[], x=0):
     if x == 0:
         return 0
     else:
-        for i in range(0, x):
-            try:
+        try:
+            for i in range(0, x):
                 print("{}".format(my_list[i]), end="")
-            except IndexError:
-                print("")
-                return i
+        except IndexError:
+            print("")
+            return i
         print("")
         return i + 1
