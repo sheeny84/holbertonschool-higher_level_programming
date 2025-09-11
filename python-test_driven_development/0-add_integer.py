6afbfbe32af3a02ab7763902c8807542e41ac368
sheeny84 <roisin.soulsby@gmail.com>
2025-09-11 15:35:13 +1000

    Update type check for task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -11,12 +11,8 @@ def add_integer(a, b=98):
     """
     Return the addition of two integers
     """
-    try:
-        a = int(a)
-    except (TypeError, ValueError):
+    if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
-    try:
-        b = int(b)
-    except (TypeError, ValueError):
+    if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
-    return a + b
+    return int(a) + int(b)
