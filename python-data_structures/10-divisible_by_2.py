8a449086a3a3cfa05e802c99d0152fd2f02e7b66
sheeny84 <roisin.soulsby@gmail.com>
2025-09-11 13:10:50 +1000

    Update task 6 and 10 for improved code

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -2,8 +2,5 @@
 def divisible_by_2(my_list=[]):
     result = []
     for i in my_list:
-        if i % 2 == 0:
-            result.append(True)
-        else:
-            result.append(False)
+        result.append(i % 2 == 0)
     return result
--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix != [[]]:
-        for i in range(0, len(matrix)):
-            for j in range(0, len(matrix[0])):
-                print("{:d}".format(matrix[i][j]), end="")
-                if j < len(matrix[0])-1:
-                    print(" ", end="")
-            print("")
-    else:
-        print("")
+    for row in matrix:
+        print(" ".join("{:d}".format(num) for num in row))
