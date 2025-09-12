a3d2a8cb4fc49303f6772cda1fd186802cf50559
sheeny84 <roisin.soulsby@gmail.com>
2025-09-12 13:11:41 +1000

    Add tests for divide by inf and no args and fix pycodestyle for task 1

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -2,10 +2,11 @@
 """
 This is the "maxtrix_divided" module.
 
-The maxtrix_divided module supplies one function, maxtrix_divided().  For example,
+The maxtrix_divided module supplies one function,
+maxtrix_divided().  For example,
 
->>> add_integer(1, 2)
-3
+>>> matrix_divided([[1, 2, 3], [4, 5, 6]], 3)
+    [[0.33, 0.67, 1.0], [1.33, 1.67, 2.0]]
 """
 
 
@@ -13,6 +14,8 @@ def matrix_divided(matrix, div):
     """
     Return a new matrix which is the result of the first matrix divided by div
     """
+    if not matrix or not div:
+        raise ValueError("matrix and div must be provided")
     if not isinstance(div, int) and not isinstance(div, float):
         raise TypeError("div must be a number")
     elif div == 0:
@@ -24,7 +27,8 @@ def matrix_divided(matrix, div):
             raise TypeError("Each row of the matrix must have the same size")
         for element in row:
             if not isinstance(element, int) and not isinstance(element, float):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    
+                raise TypeError("matrix must be a matrix (list of lists) "
+                                "of integers/floats")
+
     new_matrix = [[round(x/div, 2) for x in row] for row in matrix]
     return new_matrix
