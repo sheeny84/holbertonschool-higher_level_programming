752b1754b54ce9b888fb05e1444f5fb4945ca866
sheeny84 <roisin.soulsby@gmail.com>
2025-09-16 15:43:03 +1000

    Add files for task 2

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python3
+"""
+This is the "Rectangle" class module.
+
+"""
+
+
+class Rectangle:
+    """
+    Initialise a new instance of the Rectangle class
+    """
+    def __init__(self, width=0, height=0):
+        self.__dict__ = {}
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = width
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        elif height < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = height
+
+    @property
+    def width(self):
+        """
+        Return the width of the current rectangle
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        Set the current rectangles width
+
+        Args:
+            width (int): width of the rectangle
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """
+        Return the height of the current rectangle
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        Set the current rectangles height
+
+        Args:
+            height (int): height of the rectangle
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        """
+        Return the area of the current rectangle
+        """
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """
+        Return the perimeter of the current rectangle
+        """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return 2*self.__height + 2*self.__width
