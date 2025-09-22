28d902ed5d20a633f05af39f666990c915c7ee8c
sheeny84 <roisin.soulsby@gmail.com>
2025-09-22 17:49:09 +1000

    Add documentation to task 0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+"""
+This is the "lookup" module.
+
+"""
+
+
 def lookup(obj):
+    """
+    Return a list of available
+    attributes and methods of an object
+    """
     return dir(obj)
