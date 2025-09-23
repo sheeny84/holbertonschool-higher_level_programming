417debda5d35b7a21cf521388df4c20af2f08389
sheeny84 <roisin.soulsby@gmail.com>
2025-09-23 11:48:49 +1000

    Move import statement for task 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 """
 This is the "Rectangle" class
 
 """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
