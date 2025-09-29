e6ed3503bf3a6bee4cf0e65139248bf0e96d6af9
sheeny84 <roisin.soulsby@gmail.com>
2025-09-29 17:46:08 +1000

    Update task 0 to read each line

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -9,4 +9,5 @@ def read_file(filename=""):
     Read a text file and print it to stdout
     """
     with open(filename, encoding="utf-8") as f:
-        print(f.read())
+        for line in f:
+            print(line, end='')
