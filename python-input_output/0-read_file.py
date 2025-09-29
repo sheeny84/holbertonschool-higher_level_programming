8d90892f160a7350ac40e26de2606f18af947c9c
sheeny84 <roisin.soulsby@gmail.com>
2025-09-29 17:42:05 +1000

    Add documentation to task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""
+This is the read_file module
+"""
+
 
 def read_file(filename=""):
+    """
+    Read a text file and print it to stdout
+    """
     with open(filename, encoding="utf-8") as f:
         print(f.read())
