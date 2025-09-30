12191561f563a8246ea4d242786263ec20299262
sheeny84 <roisin.soulsby@gmail.com>
2025-09-30 11:43:45 +1000

    Update task 8 for pycodestyle

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -8,9 +8,7 @@ import json
 def class_to_json(obj):
     """
     Return the dictionary description with simple data structure
-    (list, dictionary, string, integer and boolean) for JSON serialization of an object:
+    (list, dictionary, string, integer and boolean) for JSON
+    serialization of an object.
     """
     return obj.__dict__
-
-
-    
