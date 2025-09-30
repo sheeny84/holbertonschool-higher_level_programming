bc947f06027a315dac27fab8cd6de377f41c8fb1
sheeny84 <roisin.soulsby@gmail.com>
2025-09-30 10:51:01 +1000

    Add files for task 3

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This is the to_json_string module
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Return the JSON representation of an object
+    """
+    return json.dumps(my_obj)
