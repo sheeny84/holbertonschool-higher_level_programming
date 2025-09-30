7df10d2008ad2cc1d552d726d5dbc030dd133bfa
sheeny84 <roisin.soulsby@gmail.com>
2025-09-30 11:42:40 +1000

    Add files for task 8

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This is the class_to_json module
+"""
+import json
+
+
+def class_to_json(obj):
+    """
+    Return the dictionary description with simple data structure
+    (list, dictionary, string, integer and boolean) for JSON serialization of an object:
+    """
+    return obj.__dict__
+
+
+    
