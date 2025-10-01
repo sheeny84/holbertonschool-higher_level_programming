11b80999f7efdd583d68e2d5651f9b67cbde4ff6
sheeny84 <roisin.soulsby@gmail.com>
2025-10-01 14:18:33 +1000

    Update task 0 to use json not pickle

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 """ This is the Basic Serialization module
 """
-import pickle
+import json
 
 
 def serialize_and_save_to_file(data, filename):
     """ Serialize and save data to specified file
     """
-    with open(filename, mode='wb') as f:
-        pickle.dump(data, f)
+    with open(filename, "w") as f:
+        json.dump(data, f)
 
 
 def load_and_deserialize(filename):
     """ Deserialize data from specified file
     """
-    with open(filename, "rb") as f:
-        return pickle.load(f)
+    with open(filename, "r") as f:
+        return json.load(f)
