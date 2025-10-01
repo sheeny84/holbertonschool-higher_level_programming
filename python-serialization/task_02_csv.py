2d7289850b7b624899046817c3d2cadadb69c25d
sheeny84 <roisin.soulsby@gmail.com>
2025-10-01 15:20:48 +1000

    Add return bool for task 2

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -7,9 +7,14 @@ import json
 
 def convert_csv_to_json(filename):
     """ Convert csv file to json"""
-    # open the csv file and read the data
-    with open(filename, mode='r', encoding='utf-8') as csvfile:
-        data = list(csv.DictReader(csvfile))
+    try:
+        # open the csv file and read the data
+        with open(filename, mode='r', encoding='utf-8') as csvfile:
+            data = list(csv.DictReader(csvfile))
 
-    with open('data.json', mode='w', encoding='utf-8') as jsonfile:
-        json.dump(data, jsonfile, indent=4)
+        with open('data.json', mode='w', encoding='utf-8') as jsonfile:
+            json.dump(data, jsonfile, indent=4)
+        
+        return True
+    except Exception:
+        return False
