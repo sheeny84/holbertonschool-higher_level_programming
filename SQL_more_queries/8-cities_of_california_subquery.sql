c55ca9ca05cda97a9b7072012a75abe7152aa53b
sheeny84 <roisin.soulsby@gmail.com>
2025-10-22 17:19:04 +1100

    Remove use database from task 8

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,5 @@
 -- list all the cities of CAlifornia found in database
-USE hbtn_0d_usa;
 SELECT * FROM cities
 WHERE state_id = (
 	SELECT id FROM states
-	WHERE name ='California')
+	WHERE name='California')
