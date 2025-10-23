27ec6b3ec4057342b1e8c8df8c80e79fd2d4f41b
sheeny84 <roisin.soulsby@gmail.com>
2025-10-23 15:52:05 +1100

    Add task 1 and add cur and db close to task 0

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''This is the Select States Module'''
+'''This is the Filter States Module'''
 import MySQLdb
 import sys
 
@@ -9,7 +9,7 @@ def list_all_states(username, password, name):
     db = MySQLdb.connect(host='localhost', port=3306, user=username,
                          passwd=password, db=name)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states")
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
     rows = cur.fetchall()
     for row in rows:
         print(row)
