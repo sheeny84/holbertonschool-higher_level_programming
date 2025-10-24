bcc10ede4fe62d2a080bd2b5b0c5511bae50269a
sheeny84 <roisin.soulsby@gmail.com>
2025-10-24 11:12:23 +1100

    Fix task 4 to list state name not id

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -4,12 +4,15 @@ import MySQLdb
 import sys
 
 
-def list_all_cities(username, password, name):
-    ''' This function lists all the cities in the database '''
+def cities_by_state(username, password, name):
+    ''' This function lists all the cities and their state '''
     db = MySQLdb.connect(host='localhost', port=3306, user=username,
                          passwd=password, db=name)
     cur = db.cursor()
-    query = "SELECT * FROM cities ORDER BY cities.id"
+    query = "SELECT cities.id, cities.name, states.name " \
+            "FROM cities " \
+            "JOIN states ON cities.state_id = states.id " \
+            "ORDER BY cities.id"
     cur.execute(query)
     rows = cur.fetchall()
     for row in rows:
@@ -19,4 +22,4 @@ def list_all_cities(username, password, name):
 
 
 if __name__ == "__main__":
-    list_all_cities(sys.argv[1], sys.argv[2], sys.argv[3])
+    cities_by_state(sys.argv[1], sys.argv[2], sys.argv[3])
