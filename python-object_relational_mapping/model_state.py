c6f02c7e3b70b030df21016adcac6142d84bd730
sheeny84 <roisin.soulsby@gmail.com>
2025-10-24 11:39:39 +1100

    Add task 6

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''This is the State Model Module '''
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128))
