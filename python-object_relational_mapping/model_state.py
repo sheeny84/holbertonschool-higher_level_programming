e465846b430eeea0171506d5422ab1bd4aad862c
sheeny84 <roisin.soulsby@gmail.com>
2025-10-24 11:40:41 +1100

    Add class documentation for task 6

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -7,6 +7,7 @@ Base = declarative_base()
 
 
 class State(Base):
+    '''This is the State class which inherits from Base'''
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
     name = Column(String(128))
